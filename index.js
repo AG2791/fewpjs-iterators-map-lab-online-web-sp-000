657bc79e28bb4f3cf69852c9f4c1a9be49859739
AJ Guiles <ashiguiles@gmail.com>
2020-02-16 02:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
   return tutorials.map(function (tutorial){
      tutorial = tutorial.toLowerCase().split(" ");
-     for (let i = 0, i < tutorial.length; i++) {
+     for (let i = 0; i < tutorial.length; i++) {
        tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
      }
      return tutorial.join(' ');
